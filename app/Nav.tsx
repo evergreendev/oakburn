059fdb710c8519d04eb9641b86e8707e9a8dbd8f
Joe simpson <joe@egmrc.com>
2024-05-22 11:32:40 -0600

    Add address to nav

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -15,9 +15,6 @@ const Nav = () => {
         window.addEventListener('scroll', handleScroll, {passive: true});
     }, []);
 
-    useEffect(() => {
-
-    }, [scrollPosition]);
 
     return <div className={`
     transition-colors
@@ -39,6 +36,9 @@ const Nav = () => {
             </Link>
             <Link className="hover:text-blue-300" href="/menu">Menu</Link>
             <Link className="hover:text-blue-300" href="/catering">Catering</Link>
+            <address><Link className="hover:text-blue-300"
+                           href="https://www.google.com/maps/dir/44.0800678,-103.2231332/821+Fairmont+Blvd,+Rapid+City,+SD+57701/@44.0699232,-103.2489432,14z/data=!3m1!4b1!4m18!1m8!3m7!1s0x877d43170d49759f:0x1ce8abfc3781e8c3!2s821+Fairmont+Blvd,+Rapid+City,+SD+57701!3b1!8m2!3d44.0581714!4d-103.2331913!16s%2Fg%2F11c89362fq!4m8!1m1!4e1!1m5!1m1!1s0x877d43170d49759f:0x1ce8abfc3781e8c3!2m2!1d-103.2331913!2d44.0581714?entry=ttu">821
+                Fairmont Blvd Rapid City SD</Link></address>
         </div>
     </div>
 }
